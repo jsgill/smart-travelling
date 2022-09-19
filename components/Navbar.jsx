2eb461c8a2226e3113e3ce73cc0cc17fd79376e0
Mukesh97297 <mkverma97297@gmail.com>
2022-09-19 13:48:41 +0530

    add navbar component in small letter

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function Navbar() {
-    return (
-        <div>Navbar</div>
-    )
+function navbar() {
+  return (
+    <div>navbar dfeg;ewgnfqewfeqw</div>
+  )
 }
 
-export default Navbar
+export default navbar
