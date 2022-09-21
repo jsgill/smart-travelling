76d21a3a80ee5cacc38c48ad03f7c5db0972b49f
seemakaur11 <renudalal1931@gmail.com>
2022-09-21 11:26:53 +0530

    tripOne section first input again

--- components/planTrip.jsx
+++ components/planTrip.jsx
@@ -1,19 +1,26 @@
-import Link from 'next/link'
-import React from 'react'
-import styles from '../styles/Plantrip.module.css'
+import Link from "next/link";
+import React from "react";
+import styles from "../styles/Plantrip.module.css";
 function PlanTrip() {
   return (
     <>
-    <div className={styles.home_PlanTrip}>
+      <div className={styles.home_PlanTrip}>
         <div className={styles.home_bestTrip}>
-            <h1>YOUR BEST TRIP,<br /> UNIQUE TO YOUR NEEDS, JUST FEW CLICKS AWAY</h1>
+          <h1>
+            YOUR BEST TRIP,
+            <br /> UNIQUE TO YOUR NEEDS, JUST FEW CLICKS AWAY
+          </h1>
         </div>
-    </div>
-    <div className={styles.home_planButton}>
-        <Link href="/"><a className='btn' id={styles.home_planYourTrip} type="button"><span>Plan your Trip</span></a></Link>
-    </div>
+      </div>
+      <div className={styles.home_planButton}>
+        <Link href="/tripOne">
+          <a className="btn" id={styles.home_planYourTrip} type="button">
+            <span>Plan your Trip</span>
+          </a>
+        </Link>
+      </div>
     </>
-  )
+  );
 }
 
-export default PlanTrip
+export default PlanTrip;
