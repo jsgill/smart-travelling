16c8587ba672d5047be10ac729eb53ef588ac962
seemakaur11 <renudalal1931@gmail.com>
2022-09-30 12:54:15 +0530

    budget popup responsive again

--- pages/tripTwo.jsx
+++ pages/tripTwo.jsx
@@ -413,85 +413,79 @@ function TripTwo() {
                       display: input2 ? "block" : "none",
                     }} id={styles.budget_popup}>
 
-                      <div className={styles.modal}>
 
-                        <div className="row justify-content-center">
+                      <div className="row justify-content-center">
 
-                          <p className={styles.tripTwo_cross_para} onClick={toggleInput2}>
-                            <Image
-                              src={cross}
-                              alt="cross"
-                              height={12}
-                              width={15}
-                              className={styles.cross_image}
-                            />
-                          </p>
-                          <div className={styles.tripTwo_inputTwo_input_container}>
-                            <input
-                              placeholder="Enter Amount"
-                              onChange={handleAmount}
-                              className={styles.tripTwo_inputTwo_input_container_input}
-                              id={styles.enter_amount_input}
-                            />
-                          </div>
-                          <div className={styles.tripTwo_input_container}>
-                            <input
-                              className={`${amount === "5,000"
-                                ? styles.TripTwo_btn_container_btn2
-                                : styles.TripTwo_btn_container_btn
-                                }`}
-                              onClick={(e) => handleBudgetAmount(e)}
-                              value="5,000"
-                              onChange={demo}
-                              readOnly
-                              id={styles.TripTwo_input_container_btn}
-                            />
-                            <input
-                              className={`${amount === "10,000"
-                                ? styles.TripTwo_btn_container_btn2
-                                : styles.TripTwo_btn_container_btn
-                                }`}
-                              onClick={(e) => handleBudgetAmount(e)}
-                              value="10,000"
-                              onChange={demo}
-                              readOnly
-                              id={styles.TripTwo_input_container_btn}
-                            />
-                            <input
-                              className={`${amount === "15,000"
-                                ? styles.TripTwo_btn_container_btn2
-                                : styles.TripTwo_btn_container_btn
-                                }`}
-                              onClick={(e) => handleBudgetAmount(e)}
-                              value="15,000"
-                              onChange={demo}
-                              readOnly
-                              id={styles.TripTwo_input_container_btn}
-                            />
-                            <input
-                              className={`${amount === "20,000"
-                                ? styles.TripTwo_btn_container_btn2
-                                : styles.TripTwo_btn_container_btn
-                                }`}
-                              onClick={(e) => handleBudgetAmount(e)}
-                              value="20,000"
-                              onChange={demo}
-                              readOnly
-                              id={styles.TripTwo_input_container_btn}
-                            />
-                          </div>
-                          <div className={styles.tripTwo_inputtwo_btn_container}>
-                            <button
-                              className={styles.tripTwo_inputtwo_btn}
-                              onClick={saveBtn}
-                            >
-                              Save
-                            </button>
-
-                          </div>
-
-
-
+                        <p className={styles.tripTwo_cross_para} onClick={toggleInput2}>
+                          <Image
+                            src={cross}
+                            alt="cross"
+                            height={12}
+                            width={15}
+                            className={styles.cross_image}
+                          />
+                        </p>
+                        <div className="text-center mb-5 mt-2">
+                          <input
+                            placeholder="Enter Amount"
+                            onChange={handleAmount}
+                            className={styles.tripTwo_inputTwo_input_container_input}
+                            id={styles.enter_amount_input}
+                          />
+                        </div>
+                        <div id={styles.budget_btn_div}>
+                          <input
+                            className={`${amount === "5,000"
+                              ? styles.TripTwo_btn_container_btn2
+                              : styles.TripTwo_btn_container_btn
+                              }`}
+                            onClick={(e) => handleBudgetAmount(e)}
+                            value="5,000"
+                            onChange={demo}
+                            readOnly
+                            id={styles.TripTwo_input_container_btn}
+                          />
+                          <input
+                            className={`${amount === "10,000"
+                              ? styles.TripTwo_btn_container_btn2
+                              : styles.TripTwo_btn_container_btn
+                              }`}
+                            onClick={(e) => handleBudgetAmount(e)}
+                            value="10,000"
+                            onChange={demo}
+                            readOnly
+                            id={styles.TripTwo_input_container_btn}
+                          />
+                          <input
+                            className={`${amount === "15,000"
+                              ? styles.TripTwo_btn_container_btn2
+                              : styles.TripTwo_btn_container_btn
+                              }`}
+                            onClick={(e) => handleBudgetAmount(e)}
+                            value="15,000"
+                            onChange={demo}
+                            readOnly
+                            id={styles.TripTwo_input_container_btn}
+                          />
+                          <input
+                            className={`${amount === "20,000"
+                              ? styles.TripTwo_btn_container_btn2
+                              : styles.TripTwo_btn_container_btn
+                              }`}
+                            onClick={(e) => handleBudgetAmount(e)}
+                            value="20,000"
+                            onChange={demo}
+                            readOnly
+                            id={styles.TripTwo_input_container_btn}
+                          />
+                        </div>
+                        <div className={styles.tripTwo_inputtwo_btn_container}>
+                          <button
+                            className={styles.tripTwo_inputtwo_btn}
+                            onClick={saveBtn}
+                          >
+                            Save
+                          </button>
 
                         </div>
                       </div>
