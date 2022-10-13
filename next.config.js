26c7554da9ab382f30dc66d4d549d6768b572ee9
Rangeeshar <rangees28@gmail.com>
2022-10-13 18:43:16 +0530

    fix: remove custom build directory

--- next.config.js
+++ next.config.js
@@ -3,9 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    loader: 'imgix',
-    path: '/',
-  },
+        unoptimized: true
+    } 
 }
 
 module.exports = nextConfig
