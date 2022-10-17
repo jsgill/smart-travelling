15d2e8f8dd44d44b6f31dde662b0dac1ea47ab32
seemakaur11 <raiseema248@gmail.com>
2022-10-17 12:38:57 +0530

    added pdf again

--- components/Iframe.jsx
+++ components/Iframe.jsx
@@ -6,7 +6,7 @@ import cross from "../public/images/trip/cross.png";
 function Iframe({ rref, ssrc }) {
     return (
         <div id={styles.ifram11}>
-            <iframe src={ssrc} width={1200} height={600} scrolling="no" id={styles.iframe} ref={rref} ></iframe>
+            <iframe src={ssrc} scrolling="no" style={{ height: "100vh", width: "90vw" }} ref={rref} ></iframe>
             <div className={styles.cross_pdf}>
                 <Image src={cross} width="20px" height="20px" alt="cross" className={styles.img_pdf} />
             </div>
